f0e090d615698226599e0e516cc47c200225bfb0
Anders Latif <andl@kea.dk>
2021-08-31 15:05:30 +0200

    Solved Basics and added 2.Strings

--- 0._Intro/1._Intro_Exercises/1.Basics.js
+++ 0._Intro/1._Intro_Exercises/1.Basics.js
@@ -9,6 +9,14 @@ const lastName = "Latif";
 // show in the console
 // My first name is Anders and my last name is Latif
 
+console.log("My first name is", firstName, "and my last name is", lastName);
+console.log(`My first name is ${firstName} and my last name is ${lastName}`);
+
+console.log('"Simon"');
+console.log("Thomas'");
+console.log(`"R'a"s"m'u"s`);
+
+// type inference
 
 // --------------------------------------
 // Exercise 2 - Numbers and Strings
@@ -19,6 +27,15 @@ const number = 1;
 // Add the year plus the number
 // The result should be 2021
 // You cannot touch line 1 or 2
+
+// const newYear = parseInt(year) + number;
+// const newYear = Number(year) + number;
+const newYear = +year + number;
+
+console.log(newYear);
+
+console.log(parseInt("102a43434")); // parses as 102 stops before a
+console.log(Number("102a")); // this is obviously not a Number so 102
 
 
 // --------------------------------------
