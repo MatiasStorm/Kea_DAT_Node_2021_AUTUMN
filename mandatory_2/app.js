69c8063ca2fbcabc47c3eb32440f8a8f72db26be
MatiasStorm <mat-435@hotmail.com>
2021-11-15 17:56:10 +0100

    Add nodefolie to mandatory_2 folder !

---
+++ mandatory_2/app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/* Import and use routes */
+const projectsRouter = require("./routers/projects.js");
+const pagesRouter = require("./routers/pages.js")
+const contactRouter = require("./routers/contact.js");
+
+app.use(projectsRouter.router);
+app.use(pagesRouter.router);
+app.use(contactRouter.router);
+
+const { createPage } = require("./render.js");
+const { urlencoded } = require("express");
+
+/* Ready the pages */
+const frontpagePage = createPage("frontpage/frontpage.html", { 
+        title: "Nodefolio | Welcome"
+});
+const CVPage = createPage("CVPage/CVPage.html");
+const projectsPage = createPage("projects/projects.html");
+const contactPage = createPage("contact/contact.html");
+
+app.get("/", (req, res) => {
+    res.send(frontpagePage);
+});
+
+app.get("/cv", (req, res) => {
+    res.send(CVPage);
+});
+
+app.get("/projects", (req, res) => {
+    res.send(projectsPage);
+});
+
+app.get("/contact", (req, res) => {
+    res.send(contactPage);
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, (error) => {
+    console.log("Server is running on", PORT);
+});
