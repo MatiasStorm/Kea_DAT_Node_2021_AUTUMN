dd62e33f8749ab96a2bf4946d59d7869bdab32fb
MatiasStorm <mat-435@hotmail.com>
2021-11-21 15:33:17 +0100

    Add post functionallity to dashboard

--- mandatory_2/public/pages/admin/dashboard/dashboard.js
+++ mandatory_2/public/pages/admin/dashboard/dashboard.js
@@ -1,3 +1,18 @@
+fetch("/api/technologies")
+    .then( response => response.json() )
+    .then( technologies => {
+        const technologiesSelector = document.getElementById("technologies");
+
+        technologies.forEach(technology => { 
+            const technologyOption = document.createElement("option");
+            technologyOption.innerHTML = `
+                ${technology.name}
+            `;
+            technologyOption.value = technology.name;
+            technologiesSelector.appendChild(technologyOption);
+        });
+    } );
+
 fetch("/api/projects")
     .then(response => response.json())
     .then(( projects ) => {
@@ -26,5 +41,31 @@ async function deleteProject(id){
     await fetch(`/api/projects/${id}`, {
         method: "DELETE"
     })
+    window.location.reload();
+}
+
+async function saveProject(){
+    const name = document.getElementById("name").value
+    const category = document.getElementById("category").value
+    const technologies = [];
+    const options = document.getElementById("technologies").options;
+    for(let i = 0; i < options.length; i++){
+        if(options[i].selected){
+            technologies.push({name: options[i].value });
+        }
+    }
+    const project = {
+        name,
+        category,
+        technologies
+    };
+
+    await fetch("/api/projects", {
+        method: "POST",
+        body: JSON.stringify(project),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
     window.location.reload();
 }
