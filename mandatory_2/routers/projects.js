bfae0e19ce000c9f24122f7f3ac1f7f157e70720
MatiasStorm <mat-435@hotmail.com>
2021-11-21 14:05:01 +0100

    Fix put and post project endpoints

--- mandatory_2/routers/projects.js
+++ mandatory_2/routers/projects.js
@@ -1,12 +1,7 @@
 const router = require("express").Router();
 const middleware = require("../middleware.js");
 const { Project, Technology } =  require( "../models" );
-
-const projects = [
-    { name: "Node.js Recap", category: "Node.js", technologies: ["Node.js", "Html", "CSS"] },
-    { name: "Nodefolio", category: "Node.js", technologies: ["Node.js", "Html", "CSS"] },
-    { name: "Adventure XP", category: "Java", technologies: ["Java", "Thymeleaf", "CSS", "MySQL"] }
-];
+const { Op } = require("sequelize");
 
 router.get("/api/projects", async (req, res) => {
     const projects = await Project.findAll({
@@ -19,7 +14,7 @@ router.get("/api/projects", async (req, res) => {
 
 
 router.get("/api/projects/:id", async (req, res) => {
-    const projects = await Project.findAll({
+    const projects = await Project.findOne({
         include: [
             Technology
         ],
@@ -31,19 +26,58 @@ router.get("/api/projects/:id", async (req, res) => {
 });
 
 router.post("/api/projects", async (req, res) => {
-    const project = await Project.create(
-        req.body, {
-            include: [
-                Technology
-            ]
+    let project = await Project.create(req.body)
+
+    project.dataValues.technologies = [];
+    let technology;
+    for(let t of req.body.technologies){
+        technology = await Technology.findOne({ where: {[Op.or]: [ {id: t.id || -1}, {name: t.name} ] }});
+        if(!technology){
+            technology = await Technology.create(t);
         }
-    );
+        await project.addTechnology(technology);
+        project.dataValues.technologies.push(technology);
+    }
+
     res.statusCode = 201;
     res.json(project);
 });
 
-router.put("/api/projects/:id", (req, res) => {
-    res.statusCode(201);
+router.put("/api/projects/:id", async (req, res) => {
+    const project = await Project.findOne({where: {id: req.params.id}, include: [ Technology ]});
+
+    if(!project){
+        res.statusCode = 404;
+        return res.send();
+    }
+
+    let technology;
+    let technologies = []
+    for(let existingTechnology of project.technologies){
+        index = req.body.technologies.findIndex(t => t.id == existingTechnology.id);
+        if(index != -1){
+            technologies.push(existingTechnology);
+        }
+        else {
+            project.removeTechnology(existingTechnology);
+        }
+    }
+
+    for(let t of req.body.technologies){
+        technology = await Technology.findOne({ where: {[Op.or]: [ {id: t.id || -1}, {name: t.name} ] }});
+        if(!technology){
+            technology = await Technology.create(t);
+        }
+        await project.addTechnology(technology);
+        technologies.push(technology);
+    }
+
+    req.body.technologies = technologies;
+
+    await project.update(req.body);
+
+    res.statusCode = 201;
+    res.json(project);
 });
 
 router.delete("/api/projects/:id", async (req, res) => {
